29c886b10d0b39a465a487740cadecd112d4118b
T K Sourabh <sourabhtk37@gmail.com>
2016-08-06 16:36:38 +0530

    test commit

--- test-dff.py
+++ test-dff.py
@@ -1,15 +1,21 @@
-
-
+from __future__ import division
 from hackutils import sh
 
 
 a=sh('date')
+
 a=a.split(' ')
 print(a)
+
 time=a[4]
 time=time.split(":")
 print(time)
-sh('cd /home/sourabh/Downloads/wallpaper')
+
+if (int(time[1]) / 10 ) == 0:
+	sh('sh wallpaper-change.sh')
+
+
+
  
 #if time[1] <= 20
 #	sh(gsettings set org.gnome.desktop.background picture-uri file:///home/serrano/Pictures/y.jpg)
